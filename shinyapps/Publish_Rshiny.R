b8fa6ea59d3cd52bbcac4493f496bf9cc25b88a4
AoxLiu <64894896+AoxLiu@users.noreply.github.com>
2023-11-11 21:36:17 -0500

    Create Publish_Rshiny.R

---
+++ shinyapps/Publish_Rshiny.R
@@ -0,0 +1,6 @@
+## Publish
+
+library(rsconnect)
+setwd("/Users/aoxliu/Documents/Project1_Lifetime_Reproductive_Success/Manuscript_1/Rshiny/Publish_NHB_2023Nov11")
+deployApp(appName="DiseaseSpecificLRS")
+
